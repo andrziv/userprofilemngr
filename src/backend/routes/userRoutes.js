f9e14e15646425e488499079dc9492bff03d28d4
andrziv <soveryhappyface@gmail.com>
2024-11-20 12:51:33 -0500

    Removed unused files

--- src/backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUserProfile } = require('../controllers/userController');
-
-const router = express.Router();
-
-// Route to get user profile by ID
-router.get('/user/:userId', getUserProfile);
-
-module.exports = router;
